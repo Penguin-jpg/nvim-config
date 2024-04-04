a65f7ce6d01bd43dbd532c489e5ca3d6608d9ec8
Penguin.jpg <sky026912@gmail.com>
2024-04-04 20:59:46 +0800

    update treesitter config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -7,8 +7,10 @@ return {
     -- add more things to the ensure_installed table protecting against community packs modifying it
     opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
       "lua",
-      "vim",
-      -- add more arguments for adding more treesitter parsers
+      "c",
+      "cpp",
+      "python",
+      -- "javascript",
     })
   end,
 }
