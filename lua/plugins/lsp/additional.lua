37cf29dfea7fa236e1829785ea45855bbc72eaac
Penguin.jpg <sky026912@gmail.com>
2024-04-05 19:14:05 +0800

    additional lsp features

---
+++ lua/plugins/lsp/additional.lua
@@ -0,0 +1,33 @@
+-- Plugins to add additional functionality for LSP
+return {
+  -- Signature help
+  {
+    "ray-x/lsp_signature.nvim",
+    event = "BufRead",
+    config = function() require("lsp_signature").setup() end,
+  },
+  -- Additional features for Clangd
+  {
+    "p00f/clangd_extensions.nvim",
+    lazy = true,
+    dependencies = {
+      "AstroNvim/astrocore",
+      opts = {
+        autocmds = {
+          clangd_extensions = {
+            {
+              event = "LspAttach",
+              desc = "Load clangd_extensions with clangd",
+              callback = function(args)
+                if assert(vim.lsp.get_client_by_id(args.data.client_id)).name == "clangd" then
+                  require "clangd_extensions"
+                  vim.api.nvim_del_augroup_by_name "clangd_extensions"
+                end
+              end,
+            },
+          },
+        },
+      },
+    },
+  },
+}
--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -3,21 +3,6 @@
 
 ---@type LazySpec
 return {
-
-  -- == Examples of Adding Plugins ==
-
-  "andweeb/presence.nvim",
-  {
-    "ray-x/lsp_signature.nvim",
-    event = "BufRead",
-    config = function() require("lsp_signature").setup() end,
-  },
-
-  -- == Examples of Overriding Plugins ==
-
-  -- You can disable default plugins as follows:
-  { "max397574/better-escape.nvim", enabled = false },
-
   -- You can also easily customize additional setup of plugins that is outside of the plugin's setup call
   {
     "L3MON4D3/LuaSnip",
