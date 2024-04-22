8f00163c7d0a8f2fd5550be93485627339bca3a4
Penguin.jpg <sky026912@gmailcom>
2024-04-22 10:51:39 +0800

    add an autocommand to restore session

--- lua/core/autocmds.lua
+++ lua/core/autocmds.lua
@@ -1,15 +1,16 @@
 -- Some commands that I want to execute in specific timing
-vim.api.nvim_create_augroup("autocommands", { clear = true })
+vim.api.nvim_create_augroup("auto_commands", { clear = true })
+vim.api.nvim_create_augroup("restore_seesion", { clear = true })
 
 vim.api.nvim_create_autocmd("InsertEnter", {
   desc = "Disable auto insert comment header",
-  group = "autocommands",
+  group = "auto_commands",
   command = "set formatoptions-=cro",
 })
 
 vim.api.nvim_create_autocmd("CursorHold", {
   desc = "Show diagnostics text when cursor hovers that line",
-  group = "autocommands",
+  group = "auto_commands",
   callback = function()
     local opts = {
       focusable = false,
@@ -21,4 +22,19 @@ vim.api.nvim_create_autocmd("CursorHold", {
     }
     vim.diagnostic.open_float(nil, opts)
   end,
+})
+
+vim.api.nvim_create_autocmd("VimEnter", {
+  desc = "Restore previous directory session if neovim opened with no arguments",
+  nested = true, -- trigger other autocommands as buffers open
+  callback = function()
+    -- Only load the session if nvim was started with no args
+    if vim.fn.argc(-1) == 0 then
+      -- try to load a directory session using the current working directory
+      require("resession").load(
+        vim.fn.getcwd(),
+        { dir = "dirsession", silence_errors = true }
+      )
+    end
+  end
 })
