522929502702ffb049218145b8af06e418d605b8
Penguin.jpg <sky026912@gmail.com>
2024-06-06 01:28:03 +0800

    add what I forgot to add in template, enable lua formatting

--- lua/plugins/configs/lsp/formatting.lua
+++ lua/plugins/configs/lsp/formatting.lua
@@ -1,12 +1,12 @@
 -- Customize lsp formatting options
-return {-- control auto formatting on save
+return {                -- control auto formatting on save
   format_on_save = {
-    enabled = true, -- enable or disable format on save globally
+    enabled = true,     -- enable or disable format on save globally
     allow_filetypes = { -- enable format on save for specified filetypes only
       -- "go",
     },
     ignore_filetypes = { -- disable format on save for specified filetypes
-      "lua",
+      -- "lua",
     },
   },
   disabled = { -- disable formatting capabilities for the listed language servers
