8cf28f0df0a7385bc5a6b2fdf412761cef0ab37e
Penguin.jpg <sky026912@gmail.com>
2024-06-13 12:43:33 +0800

    update options

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -1,23 +1,29 @@
 return {
   -- vim.opt.<key>
   opt = {
-    relativenumber = true,                                   -- sets vim.opt.relativenumber
-    number = true,                                           -- sets vim.opt.number
-    spell = false,                                           -- sets vim.opt.spell
-    signcolumn = "yes",                                      -- always show the signcolumn, otherwise it would shift the text each time
-    wrap = false,                                            -- disable auto wrap lines
+    number = true, -- show line number
+    relativenumber = true, -- show relative line number
+    spell = false, -- disable spell check
+    wrap = false, -- disable auto wrap lines
+    signcolumn = "yes", -- show changes of file
+    colorcolumn = "80", -- a line in the position of 80 chars
+    foldcolumn = "1", -- show foldcolumn
+    foldenable = true, -- enable fold for nvim-ufo
+    foldlevel = 99, -- set high foldlevel for nvim-ufo
+    foldlevelstart = 99, -- start with all code unfolded
     guicursor = "n-v-c-sm:block,i-ci-ve:ver25,r-cr-o:hor20", -- default cursor setting
-    clipboard = "unnamedplus",                               -- enable system clipboard
-    termguicolors = true,                                    -- true color support
-    mouse = "a",                                             -- enable mouse
-    mousemoveevent = true,                                   -- enable mousemove event
-    laststatus = 2,                                          -- only show one statusline
-    swapfile = false,                                        -- don't use swapfile
-    shiftwidth = 4,                                          -- number of space inserted for indentation; when zero the 'tabstop' value will be used
-    tabstop = 4,                                             -- set the number of space in a tab to 4
-    softtabstop = 4,                                         -- can be differnt from tabstop
-    showtabline = 2,                                         -- always show tabline
-    expandtab = true,                                        -- use spaces instead of tab
+    clipboard = "unnamedplus", -- enable system clipboard
+    termguicolors = true, -- true color support
+    mouse = "a", -- enable mouse
+    mousemoveevent = true, -- enable mousemove event
+    laststatus = 3, -- only show one statusline
+    swapfile = false, -- don't use swapfile
+    shiftwidth = 4, -- number of space inserted for indentation; when zero the 'tabstop' value will be used
+    tabstop = 4, -- set the number of space in a tab to 4
+    softtabstop = 4, -- can be differnt from tabstop
+    showtabline = 2, -- always show tabline
+    expandtab = true, -- use spaces instead of tab
+    undofile = true, -- enable persistent undo
   },
   -- vim.g.<key>
   g = {
