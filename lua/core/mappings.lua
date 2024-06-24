f9fff95d579b18edcc3a0257b247c8b1755565b6
Penguin.jpg <sky026912@gmail.com>
2024-06-25 00:13:16 +0800

    separate cut and delete motion

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -5,28 +5,36 @@
 -- automatically pick-up stored data by this setting.)
 
 return function()
-  local utils = require "astrocore"
-  local is_available = utils.is_available
-  local mappings = utils.empty_map_table()
+  local mappings = require("astrocore").empty_map_table()
 
-  ------ Disable default mappings ------
+  -------------------------------------------
+  ------- Disable default mappings ----------
+  -------------------------------------------
   -- Disable defaults ctrl + arrow keys
   mappings.n["<C-Left>"] = false
   mappings.n["<C-Right>"] = false
   mappings.n["<C-Up>"] = false
   mappings.n["<C-Down>"] = false
 
-  ------ Utility functions ------
+  -------------------------------------------
+  ----------- Utility functions -------------
+  -------------------------------------------
   mappings.n["<C-z>"] = { "u", desc = "Undo" }
   mappings.i["<C-z>"] = { "<C-o>u", desc = "Undo" }
-  -- <C-o> will exit insert mode tempoarily and back to insert mode again
   mappings.i["<C-Del>"] = { "<C-o>dw", desc = "Delete a word backward" }
   mappings.i["<C-s>"] = { "<Cmd>w!<CR>", desc = "Save file" }
-  mappings.v["<C-c>"] = { "y", desc = "Copy selected block" }
   mappings.i["<S-Tab>"] = { "<C-d>", desc = "Unindent line" }
-  -- Delete without yanking
-  mappings.n["D"] = { '"_dd', desc = "Delete without yanking" }
-  mappings.v["D"] = { '"_d', desc = "Delete without yanking" }
+  mappings.v["<S-Tab>"] = { "<C-d>", desc = "Unindent line" }
+
+  -- Separate cut and delete motion
+  for key, map in pairs {
+    ["d"] = { '"_d', desc = "Delete (without yanking)" },
+    ["X"] = { "d", desc = "Cut (with yanking)" },
+  } do
+    mappings.n[key] = map
+    mappings.v[key] = map
+  end
+
   -- Move in insert mode
   mappings.i["<C-h>"] = { "<Left>", desc = "Move left in insert mode" }
   mappings.i["<C-l>"] = { "<Right>", desc = "Move right in insert mode" }
@@ -39,11 +47,11 @@ return function()
     ["L"] = { "$", desc = "Jump to end of line" },
     ["K"] = { "5k", desc = "Move up 5 lines" },
     ["J"] = { "5j", desc = "Move down 5 lines" },
-    ["ga"] = { "ggVG", desc = "Select all lines" },
   } do
     mappings.n[key] = map
     mappings.v[key] = map
   end
+  mappings.n["ga"] = { "ggVG", desc = "Select all lines" }
 
   return mappings
 end
