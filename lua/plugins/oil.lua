ee10548e0d4a60df63964b2a0b3f5a6fb9fcce66
Penguin.jpg <sky026912@gmail.com>
2024-07-10 13:24:43 +0800

    replace nvim-web-icons with mini.icons

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,8 +1,33 @@
 return {
   {
+    "rebelot/heirline.nvim",
+    opts = function(_, opts)
+      local status = require "astroui.status"
+      local is_oil = function(bufnr) return status.condition.buffer_matches({ filetype = "^oil$" }, bufnr) end
+      local disable_winbar_cb = opts.opts.disable_winbar_cb
+      opts.opts.disable_winbar_cb = function(args)
+        if is_oil(args.buf) then return false end
+        return disable_winbar_cb(args)
+      end
+
+      if opts.winbar then
+        table.insert(opts.winbar, 1, {
+          condition = function(self) return is_oil(self.bufnr) end,
+          status.component.separated_path {
+            padding = { left = 2 },
+            max_depth = false,
+            suffix = false,
+            path_func = function() return require("oil").get_current_dir() end,
+          },
+        })
+      end
+    end,
+  },
+  {
     "stevearc/oil.nvim",
     cmd = "Oil",
     dependencies = {
+      "echasnovski/mini.icons",
       {
         "AstroNvim/astrocore",
         opts = {
@@ -48,30 +73,5 @@ return {
         ["yp"] = "actions.yank_entry",
       },
     },
-  },
-  {
-    "rebelot/heirline.nvim",
-    optional = true,
-    opts = function(_, opts)
-      local status = require "astroui.status"
-      local is_oil = function(bufnr) return status.condition.buffer_matches({ filetype = "^oil$" }, bufnr) end
-      local disable_winbar_cb = opts.opts.disable_winbar_cb
-      opts.opts.disable_winbar_cb = function(args)
-        if is_oil(args.buf) then return false end
-        return disable_winbar_cb(args)
-      end
-
-      if opts.winbar then
-        table.insert(opts.winbar, 1, {
-          condition = function(self) return is_oil(self.bufnr) end,
-          status.component.separated_path {
-            padding = { left = 2 },
-            max_depth = false,
-            suffix = false,
-            path_func = function() return require("oil").get_current_dir() end,
-          },
-        })
-      end
-    end,
   },
 }
--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -21,6 +21,23 @@ return {
       opts.background_colour = "#000000"
     end,
   },
+  -- Icons support
+  {
+    "echasnovski/mini.icons",
+    opts = function(_, opts)
+      if vim.g.icons_enabled == false then opts.style = "ascii" end
+    end,
+    lazy = true,
+    specs = {
+      { "nvim-tree/nvim-web-devicons", enabled = false, optional = true },
+    },
+    init = function()
+      package.preload["nvim-web-devicons"] = function()
+        require("mini.icons").mock_nvim_web_devicons()
+        return package.loaded["nvim-web-devicons"]
+      end
+    end,
+  },
   -- Transparent background
   {
     "xiyaowong/transparent.nvim",
