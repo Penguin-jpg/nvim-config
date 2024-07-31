ee34e13a5a59172ef9578d1767242bd34fd024a0
Penguin.jpg <sky026912@gmail.com>
2024-07-31 16:07:32 +0800

    fix the problem of mason not auto installing

--- lua/plugins/lsp/lspconfig.lua
+++ lua/plugins/lsp/lspconfig.lua
@@ -6,11 +6,50 @@ return {
     {
       "williamboman/mason-lspconfig.nvim", -- MUST be set up before `nvim-lspconfig`
       dependencies = { "williamboman/mason.nvim" },
-      opts = function()
-        return {
-          -- use AstroLSP setup for mason-lspconfig
-          handlers = { function(server) require("astrolsp").lsp_setup(server) end },
-        }
+      cmd = { "LspInstall", "LspUninstall" },
+      opts_extend = { "ensure_installed" },
+      opts = {
+        -- use AstroLSP setup for mason-lspconfig
+        ensure_installed = {},
+        handlers = { function(server) require("astrolsp").lsp_setup(server) end },
+      },
+      config = function(_, opts)
+        require("mason-lspconfig").setup(opts)
+      end,
+    },
+    {
+      "WhoIsSethDaniel/mason-tool-installer.nvim",
+      cmd = {
+        "MasonToolsInstall",
+        "MasonToolsInstallSync",
+        "MasonToolsUpdate",
+        "MasonToolsUpdateSync",
+        "MasonToolsClean",
+      },
+      opts = {
+        ensure_installed = {
+          -- lua
+          "lua-language-server",
+          "stylua",
+          "selene",
+          -- c/cpp
+          "clangd",
+          "clang-format",
+          "codelldb",
+          -- python
+          "ruff-lsp",
+          "black",
+          -- json
+          "json-lsp",
+          -- markdown
+          "marksman",
+          "prettierd",
+        },
+      },
+      config = function(_, opts)
+        local mason_tool_installer = require "mason-tool-installer"
+        mason_tool_installer.setup(opts)
+        if opts.run_on_start ~= false then mason_tool_installer.run_on_start() end
       end,
     },
   },
--- lua/plugins/lsp/mason.lua
+++ lua/plugins/lsp/mason.lua
@@ -1,53 +1,20 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    build = ":MasonUpdate",
-    cmd = {
-      "Mason",
-      "MasonInstall",
-      "MasonUninstall",
-      "MasonUninstallAll",
-      "MasonLog",
-    },
-    opts = {
-      ui = {
-        icons = {
-          package_installed = "✓",
-          package_uninstalled = "✗",
-          package_pending = "⟳",
-        },
+  "williamboman/mason.nvim",
+  build = ":MasonUpdate",
+  cmd = {
+    "Mason",
+    "MasonInstall",
+    "MasonUninstall",
+    "MasonUninstallAll",
+    "MasonLog",
+  },
+  opts = {
+    ui = {
+      icons = {
+        package_installed = "✓",
+        package_uninstalled = "✗",
+        package_pending = "⟳",
       },
     },
-  },
-  {
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
-    lazy = true,
-    cmd = {
-      "MasonToolsInstall",
-      "MasonToolsInstallSync",
-      "MasonToolsUpdate",
-      "MasonToolsUpdateSync",
-      "MasonToolsClean",
-    },
-    opts = function(_, opts)
-      opts.ensure_installed = {
-        -- lua
-        "lua-language-server",
-        "stylua",
-        "selene",
-        -- c/cpp
-        "clangd",
-        "clang-format",
-        "codelldb",
-        -- python
-        "ruff-lsp",
-        "black",
-        -- json
-        "json-lsp",
-        -- markdown
-        "marksman",
-        "prettierd",
-      }
-    end,
   },
 }
